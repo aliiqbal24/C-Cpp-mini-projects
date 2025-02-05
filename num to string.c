3e192b758227b7976f388fc99f326ebc9abae3f7
Ali Iqbal <ali.j.iqbal24@gmail.com>
2025-02-04 21:55:06 -0700

    Create num to string.c

---
+++ num to string.c
@@ -0,0 +1,11 @@
+//We need a function that can transform a number (integer) into a string.
+
+//What ways of achieving this do you know?
+
+
+
+#include <string>
+
+std::string number_to_string(int num) {
+  return std::to_string(num);
+}
